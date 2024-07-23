8776019de390d1f119e67d1a0e3428e760475aab
RFBomb <20431767+RFBomb@users.noreply.github.com>
2024-07-23 11:14:23 -0400

    Update FileProcessedEventArgs.cs
    - Add new constructors for the class to more easily populate the ProcessedFile property
    - Make the ProcessedFile property read-only

--- RoboSharp/EventArgObjects/FileProcessedEventArgs.cs
+++ RoboSharp/EventArgObjects/FileProcessedEventArgs.cs
@@ -1,20 +1,42 @@
 ﻿using System;
+using System.IO;
+using RoboSharp.Interfaces;
 
 // Do Not change NameSpace here! -> Must be RoboSharp due to prior releases
 namespace RoboSharp
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
     /// <summary>
     /// <inheritdoc cref="ProcessedFileInfo"/>
     /// </summary>
     public class FileProcessedEventArgs : EventArgs
     {
-        /// <inheritdoc cref="ProcessedFileInfo"/>
-        public ProcessedFileInfo ProcessedFile { get; set; }
+        
+        public static readonly FileProcessedEventArgs WhiteSpaceLogLine = new FileProcessedEventArgs(string.Empty);
 
-        /// <inheritdoc cref="EventArgs.EventArgs"/>
+        /// <inheritdoc cref="ProcessedFileInfo"/>
+        public ProcessedFileInfo ProcessedFile { get; }
+
         public FileProcessedEventArgs(ProcessedFileInfo file)
         {
             ProcessedFile = file;
         }
+
+        public FileProcessedEventArgs(string systemMessage)
+        {
+            ProcessedFile = new ProcessedFileInfo(systemMessage);
+        }
+
+        public FileProcessedEventArgs(FileInfo file, IRoboCommand command, ProcessedFileFlag status = ProcessedFileFlag.None)
+        {
+            ProcessedFile = new ProcessedFileInfo(file, command, status);
+        }
+
+        public FileProcessedEventArgs(DirectoryInfo dir, IRoboCommand command, long size = 0, ProcessedDirectoryFlag status = ProcessedDirectoryFlag.None)
+        {
+            ProcessedFile = new ProcessedFileInfo(dir, command, status, size);
+        }
     }
 }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
