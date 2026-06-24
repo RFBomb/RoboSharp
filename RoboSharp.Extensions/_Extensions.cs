26efce236042725b1f7587c64d2680aeada907fe
RBrenckman <20431767+RFBomb@users.noreply.github.com>
2026-06-24 12:23:13 -0400

    Strong Name all assemblies

--- RoboSharp.Extensions/_Extensions.cs
+++ RoboSharp.Extensions/_Extensions.cs
@@ -3,7 +3,7 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("RoboSharp.Extensions.UnitTests")]
+[assembly: InternalsVisibleTo("RoboSharp.Extensions.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e592a7369ca3dd96390ddfbf07df5c714be66670ed4230e4c8ecdb4ccc46072cda33676b502f119841e870aa5afc93cea9c56799a81ddab5fb44a2b9f9e93625c25aa46b51e25671f3dc971f2e44e770fb3f66bb29de8b932a6c24c40656cc43b807c5a7e532f79a86730ea8f930f020bd7757917fa45eb2e54324c60f588cc5")]
 namespace RoboSharp.Extensions
 {
 
